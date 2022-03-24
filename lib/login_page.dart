84b096d987b0dbfea7034c4c23bab9d16da321f7
Naufal Amiruddin <naufalamiruddin123456@gmail.com>
2022-03-24 21:51:28 +0700

    Update login_page.dart
    - layouting
    - menambahkan "daftar sekarang"

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -13,52 +13,62 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text("Login"),
         centerTitle: true,
       ),
       body: Form(
+        key: _formKey,
         child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            alignment: Alignment.center,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextForm(formText: "Email", hint: "example@ex.com"),
-                  SizedBox(height: 20),
-                  TextForm(
-                    formText: "Password",
-                    hint: "Masukkan password",
-                    isPasswordInput: true,
-                  ),
-                  SizedBox(height: 50),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(MyColor.oren),
-                      foregroundColor: MaterialStateProperty.all(Colors.black),
-                      fixedSize: MaterialStateProperty.all(
-                          Size(MediaQuery.of(context).size.width, 40)),
-                      padding: MaterialStateProperty.all(
-                        EdgeInsets.symmetric(vertical: 5),
-                      ),
-                      textStyle: MaterialStateProperty.all(
-                        TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          fontFamily: 'Poppins',
-                        ),
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          alignment: Alignment.bottomCenter,
+          child: SingleChildScrollView(
+            physics: ClampingScrollPhysics(),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextForm(formText: "Email", hint: "example@ex.com"),
+                SizedBox(height: 30),
+                TextForm(
+                  formText: "Password",
+                  hint: "Masukkan password",
+                  isPasswordInput: true,
+                ),
+                SizedBox(height: 70),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(MyColor.oren),
+                    foregroundColor: MaterialStateProperty.all(Colors.black),
+                    fixedSize: MaterialStateProperty.all(
+                        Size(MediaQuery.of(context).size.width, 45)),
+                    textStyle: MaterialStateProperty.all(
+                      TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
                       ),
                     ),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {}
-                    },
-                    child: Text("Masuk"),
-                  )
-                ],
-              ),
-            )),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  child: Text("Masuk"),
+                ),
+                SizedBox(height: 150),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Belum memiliki akun?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("Daftar sekarang"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
