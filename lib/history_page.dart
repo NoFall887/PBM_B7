741eda9dad86886496812b82106d346cf8b8789a
fadilabdil <fadilabdillah0711@gmail.com>
2022-04-01 15:19:17 +0700

    history pemesanan
    tampilan appbar

---
+++ lib/history_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HistoryPage extends StatelessWidget {
+  const HistoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        title: const Center(
+          child: Text("Riwayat Pemesanan"),
+        ),
+      ),
+    );
+  }
+}
